89e8769ef42bffa0be5a2866f0a161ae6bb03a54
Anders Nomerstad <anders@nomerstad.com>
2026-06-15 11:59:56 +0200

    Oppdaterer libs. Øker timeout of failureThreshold for ready probe

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val kotlinxCoroutinesVersion = "1.11.0"
 val prometheusVersion = "0.16.0"
 val apacheCommonsTextVersion = "1.15.0"
 val pdfboxVersion = "3.0.7"
-val felleslibVersion = "0.0.792"
+val felleslibVersion = "0.0.827"
 val flywayVersjon = "12.8.1"
 val testContainersVersion = "2.0.5"
 
