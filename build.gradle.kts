0472dca9bbe55c37eec535a855382536a918abf0
John Andre Hestad <john.andre.hestad@nav.no>
2026-07-22 19:41:06 +0200

    Update felleslibVersion to 0.0.20260722143244

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val kotlinxCoroutinesVersion = "1.11.0"
 val prometheusVersion = "0.16.0"
 val apacheCommonsTextVersion = "1.15.0"
 val pdfboxVersion = "3.0.8"
-val felleslibVersion = "0.0.20260718174757"
+val felleslibVersion = "0.0.20260722143244"
 val flywayVersjon = "12.10.0"
 val testContainersVersion = "2.0.5"
 
